43ef20e4bd6188ea614b642edb8a1a9b08510edb
takhozu10 <thozumi@biglots.com>
2017-09-12 09:09:04 -0400

    Initial Commit

--- configDefaultMail.py
+++ configDefaultMail.py
@@ -4,4 +4,4 @@ from LaunchServices import *
 
 LSSetDefaultHandlerForURLScheme("mailto", "com.microsoft.outlook")
 LSSetDefaultRoleHandlerForContentType("com.apple.ical.ics", kLSRolesAll, "com.microsoft.Outlook")
-LSSetDefaultRoleHandlerForContentType("public.vcard", kLSRoleAll, "com.microsoft.Outloook")
+LSSetDefaultRoleHandlerForContentType("public.vcard", kLSRolesAll, "com.microsoft.Outloook")
